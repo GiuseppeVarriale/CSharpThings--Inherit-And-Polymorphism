7415af1c710b49bf8ae0e094eda388a51050b587
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 15:10:30 -0300

    Create ImportedProduct Entity

---
+++ InheritAndPolymorphism/InheritAndPolymorphism/Entities/ImportedProduct.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace InheritAndPolymorphism.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFee { get; private set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price,double customsFee) : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            double totalPrice = Price + CustomsFee;
+            return totalPrice;
+        }
+        public override string PriceTag()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append(" $ ");
+            sb.Append(TotalPrice().ToString("F2", CultureInfo.InvariantCulture));
+            sb.Append(" (Customs fee:  $ ");
+            sb.Append(CustomsFee.ToString("F2", CultureInfo.InvariantCulture));
+            sb.Append(")");
+            return sb.ToString();
+        }
+    }
+}
