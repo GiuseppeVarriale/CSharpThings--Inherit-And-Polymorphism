64a29007bf720198b843eb987950366671a84895
Giuseppe Varriale <pepe.saoleo@gmail.com>
2019-11-20 14:28:57 -0300

    Create Product Entity

---
+++ InheritAndPolymorphism/InheritAndPolymorphism/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Globalization;
+
+namespace InheritAndPolymorphism.Entities
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string PriceTag()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Name);
+            sb.Append(" $ ");
+            sb.Append(Price.ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
